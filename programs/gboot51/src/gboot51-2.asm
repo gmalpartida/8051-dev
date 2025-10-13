04d66f8b2fe19d73e5fde142174fc4b8357f7617
Gino Malpartida <ginmal@gmail.com>
2025-10-12 23:29:15 -0400

    added uart_rx_char and uart_tx_char

--- programs/gboot51/src/gboot51-2.asm
+++ programs/gboot51/src/gboot51-2.asm
@@ -24,12 +24,11 @@ main:
 	acall uart_init_baud_gen
 
 loop_echo:
-
-	acall uart_rx_char
- 	mov sbuf, a
-	acall uart_tx_char
-
-  	sjmp loop_echo ; jump back to the beginning of the loop
+	mov dptr, #0010h
+	acall uart_rx_string
+	mov dptr, #0010h
+	acall uart_tx_string
+	sjmp $
 
 ; initializes timer 1 as standard uart baud rate generator
 ; <-- timer 1: 9600bps, mode 2, 8-bit auto-reload
@@ -52,21 +51,39 @@ uart_rx_char:
 	ret
 
 ; receives a string from the serial port
-; --> dptr: location of string that was received
+; <-- dptr: location of string that was received
 uart_rx_string:
 loop_rx_string:
 	acall uart_rx_char
-
-	cjne a, #CR, loop_rx_string
-
+	movx @dptr, a
+	cjne a, #CR, uart_rx_string_not_cr
+	mov a, #00h
+	movx @dptr, a
+	jmp exit_uart_rx_string
+uart_rx_string_not_cr:
+	inc dptr
+	jmp loop_rx_string
+exit_uart_rx_string:
 	ret
 
 ; transmits character via serial port
-; --> a: character to be sent
+; --> a: character to be transmitted
 uart_tx_char:
 	jnb ti, $
 	clr ti
+	mov sbuf, a
+	ret
 
+; transmits a string via serial port
+; --> dptr: location of string to be transmitted
+uart_tx_string:
+	movx a, @dptr
+	jz exit_uart_tx_string
+	acall uart_tx_char
+	inc dptr
+	jmp uart_tx_string
+
+exit_uart_tx_string:
 	ret
 end
 
