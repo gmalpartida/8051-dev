fbe697027296f25a70ee262771311d8768e6e2df
Gino Malpartida <ginmal@gmail.com>
2025-10-16 21:23:45 -0400

    added command handling

--- programs/gboot51/src/gboot51-2.asm
+++ programs/gboot51/src/gboot51-2.asm
@@ -1,8 +1,4 @@
-cseg
-	CR equ 0dh
-	LF equ 0ah
-	TAB equ 09h
-
+cseg at 00h
 	ljmp main
 ; space reserved for vector table
 ie0_vector:
@@ -43,6 +39,7 @@ command_prompt_loop:
 	mov dptr, #command_prompt_str
 	acall uart_tx_string_from_cseg
 
+	
 	acall get_cmd
 
 	cjne a, #HELP_CMD, process_ls_cmd
@@ -51,34 +48,69 @@ command_prompt_loop:
 	jmp command_prompt_loop
 process_ls_cmd:
 	cjne a, #LS_CMD, process_peek_cmd	
-	mov dptr, #ls_cmd_txt
-	acall uart_tx_string_from_cseg
+	acall do_process_ls_cmd
 	jmp command_prompt_loop
 	
 process_peek_cmd:
 	cjne a, #PEEK_CMD, process_poke_cmd
-	mov dptr, #peek_cmd_txt
-	acall uart_tx_string_from_cseg
+	acall do_process_peek_cmd
 	jmp command_prompt_loop
 process_poke_cmd:
 	cjne a, #POKE_CMD, process_invalid_cmd
-	mov dptr, #poke_cmd_txt
-	acall uart_tx_string_from_cseg
+	acall do_process_poke_cmd
 	jmp command_prompt_loop
-	sjmp $	
 process_invalid_cmd:
 	jmp command_prompt_loop
 show_help:
 	
 	jmp $
 
+do_process_ls_cmd:
+	mov dptr, #ls_str
+	acall uart_tx_string_from_cseg
+
+	ret
+
+do_process_peek_cmd:
+	;mov dptr, #peek_cmd_txt
+	;acall uart_tx_string_from_cseg
+	mov dptr, #uart_rx_buffer
+	acall parse_mem_address
+
+	ret
+
+do_process_poke_cmd:
+	mov dptr, #poke_cmd_txt
+	acall uart_tx_string_from_cseg
+	ret
+
+fill_xmem:
+	clr a
+fill_xmem_loop:
+	movx @dptr, a
+	inc dptr
+	inc a
+	cjne a, #00h, fill_xmem_loop
+	ret
+
+parse_mem_address:
+	movx a, @dptr
+	jz exit_parse_mem_address
+	acall skip_blanks
+	movx a, @dptr
+	
+parse_mem_address_loop:
+	
+
+exit_parse_mem_address:
+	ret
+
 $include(boot51.inc)
 $include(serial.inc)
 $include(menu.inc)
 
-xseg
+xseg at 0000h
 	uart_rx_buffer: ds 255
-
 
 end
 
